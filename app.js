517661abefec00b5e230eba71a07a46aeb808507
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-28 10:22:45 +0430

    os module

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+import { fileURLToPath } from "url";
 import { log } from "./logger.js";
 import path from "path";
+import os from "os";
 
 console.log(log("hello"));
-console.log(log(path.parse("./logger.js")));
+// console.log(log(path.parse("./logger.js")));
+
+// console.log(fileURLToPath(import.meta.url));
+
+console.log(os.totalmem());
+console.log(os.freemem());

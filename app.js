16a84cf2ae55c05d46ac0a607dfa3f26d6df78db
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-28 09:45:03 +0430

    path module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const log = require("./logger");
+import { log } from "./logger.js";
+import path from "path";
 
 console.log(log("hello"));
+console.log(log(path.parse("./logger.js")));
--- logger.js
+++ logger.js
@@ -1,10 +1,8 @@
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
 const url = "http://mylogger.io/log";
 
-function log(message) {
+export function log(message) {
   // send and http request
   console.log(message);
 }
-
-module.exports = log;

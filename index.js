f4a8cadf87b081cb0f9369d9108bf4010f1f16bf
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-28 22:16:58 +0430

    route param

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send(["Hussain", "Hamim", "Afghan", "hello!"]);
+  res.send(["js", "react", "node", "hello!"]);
 });
 
-process.env.PORT = 6000;
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+// query parameter
+app.get("/api/search", (req, res) => {
+  res.send(req.query);
+});
+
+// process.env.PORT = 6000;
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));

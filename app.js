c53e040e13e7c5b885941df2adc9895ae6c7b42d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-28 10:32:53 +0430

    file system module

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ import { fileURLToPath } from "url";
 import { log } from "./logger.js";
 import path from "path";
 import os from "os";
+import fs from "fs";
 
 console.log(log("hello"));
 // console.log(log(path.parse("./logger.js")));
 
 // console.log(fileURLToPath(import.meta.url));
 
-console.log(os.totalmem());
-console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+const files = fs.readdirSync("./");
+console.log(files);
+
+fs.readdir("./", function (err, files) {
+  if (err) console.log(err);
+  else console.log(files);
+});

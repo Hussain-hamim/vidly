068326a01f5681ff23300b8dbf39f3940102cbe7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-28 22:30:05 +0430

    handling http get req

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import express from "express";
 const app = express();
 
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello world!!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send(["js", "react", "node", "hello!"]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course with given id was not found");
+  res.send(course);
 });
 
 app.get("/api/posts/:year/:month", (req, res) => {

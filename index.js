b897cc321ddcdd3381a5185b9f37af813e9a775d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-29 10:57:58 +0430

    validate with joi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Joi from "joi";
 const app = express();
 
 app.use(express.json());
@@ -28,6 +29,17 @@ app.get("/api/posts/:year/:month", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,

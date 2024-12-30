eba804be0d7297e745c03173ba2aa308fd00bc9c
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-30 15:22:23 +0430

    creating settled promises

--- promise.js
+++ promise.js
@@ -3,14 +3,20 @@
 // promise to give the result of an async op
 //// promise state: pending, fulfilled, rejected
 
-const p = new Promise((resolve, reject) => {
-  // kick off some async work...
-  setTimeout(() => {
-    // resolve(1);
-    reject(new Error("message"));
-  }, 2000);
-});
+// const p = new Promise((resolve, reject) => {
+//   // kick off some async work...
+//   setTimeout(() => {
+//     // resolve(1);
+//     reject(new Error("message"));
+//   }, 2000);
+// });
 
-p.then((result) => console.log("result", result)).catch((error) =>
-  console.log("error", error.message)
-);
+// p.then((result) => console.log("result", result)).catch((error) =>
+//   console.log("error", error.message)
+// );
+
+const p = Promise.resolve({ id: 1 });
+const p2 = Promise.reject(new Error("reason for rejection..."));
+
+p.then((result) => console.log(result));
+p2.catch((result) => console.log(result.message));
